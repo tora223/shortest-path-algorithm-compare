2fb3e81f6f48030ab76535fa7170caf341973c01
tora223 <taitai.m.0223@ezweb.ne.jp>
2024-05-28 17:07:38 +0900

    add bfs.py

---
+++ module/bfs.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(G: list[list[int]], src: int) -> tuple[list[int], list[int]]:
+    """__description__
+
+    Args:
+        G (list[list[int]]): 隣接リスト表現のグラフ
+        src (int): 始点となるノードのインデックス
+
+    Returns:
+        tuple[list[int], list[int]}: (距離のリスト, 経路復元に使うリスト)
+    """
+    q = deque([src])
+    dist = [-1] * len(G)
+    dist[src] = 0
+    prev = [-1] * len(G)
+    
+    while q:
+        v = q.popleft()
+        for u in G[v]:
+            if dist[u] == -1:
+                dist[u] = dist[v] + 1
+                prev[u] = v
+                q.append(u)
+                
+    return dist, prev
