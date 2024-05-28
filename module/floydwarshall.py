7fb346d35b91f700918fc74035b2b2332e7a797a
tora223 <taitai.m.0223@ezweb.ne.jp>
2024-05-28 17:09:36 +0900

    add floydwarshall.py

---
+++ module/floydwarshall.py
@@ -0,0 +1,29 @@
+def floyd_warshall(G: list[list[int]]) -> tuple[list[list[int]], list[list[int]]]:
+    """__description__
+
+    Args:
+        G (list[list[int]]): 隣接行列表現のグラフ
+
+    Returns:
+        tuple[list[list[int]], list[list[int]]]: (距離のリスト, 経路復元に使うリスト)
+    """
+    nodes = len(G)
+    INF = 10**18
+    dist = [[INF]*nodes for _ in range(nodes)]
+    prev = [[-1]*nodes for _ in range(nodes)]
+    for i in range(nodes):
+        for j in range(nodes):
+            if i == j:
+                dist[i][j] = 0
+            elif G[i][j] > 0:
+                dist[i][j] = G[i][j]
+                prev[i][j] = i
+    
+    for k in range(nodes):
+        for i in range(nodes):
+            for j in range(nodes):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    prev[i][j] = prev[k][j]
+                    
+    return dist, prev
