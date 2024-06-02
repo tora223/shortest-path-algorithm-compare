56ba51cc3f859e0797ababd5ff38e626f2108d1b
tora223 <taitai.m.0223@ezweb.ne.jp>
2024-06-03 00:22:01 +0900

    change way of time masurement in main.py

--- main.py
+++ main.py
@@ -15,102 +15,97 @@ def main():
     print(f"Resolution: {info.resolution} seconds")
     
     print(f"Nodes: {data.nodes}, Edges: {data.edges}")
+    
 
     # 単一始点最短経路
     print("単一始点最短経路")
-    N = 10 # 試行回数
     
     # bfs
-    time_sec = 0
-    for i in range(N):
-        start = time.perf_counter()
-        G = data.get_graph_list()
-        bfs(G, 0)
-        end = time.perf_counter()
-        time_sec += end-start
-    print(f"幅優先探索: {time_sec/N:0.7f} sec")
+    G = data.get_graph_list()
+    start = time.perf_counter()
+    bfs(G, 0)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"幅優先探索: {time_sec:0.7f} sec")
     
     # dijkstra (二分ヒープ)
-    time_sec = 0
-    for i in range(N):
-        start = time.perf_counter()
-        G = data.get_graph_with_weight()
-        dijkstra(G, 0)
-        end = time.perf_counter()
-        time_sec += end-start
-    print(f"ダイクストラ (二分ヒープ): {time_sec/N:0.7f} sec")
+    G = data.get_graph_with_weight()
+    start = time.perf_counter()
+    dijkstra(G, 0)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"ダイクストラ (二分ヒープ): {time_sec:0.7f} sec")
     
     # dijkstra (隣接行列)
-    time_sec = 0
-    for i in range(N):
-        start = time.perf_counter()
-        G = data.get_graph_matrix()
-        dijkstra(G, 0, PQ=False)
-        end = time.perf_counter()
-        time_sec += end-start
-    print(f"ダイクストラ (隣接行列): {time_sec/N:0.7f} sec")
+    G = data.get_graph_matrix()
+    start = time.perf_counter()
+    dijkstra(G, 0, PQ=False)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"ダイクストラ (隣接行列): {time_sec:0.7f} sec")
     
     # bellman-ford
-    time_sec = 0
-    for i in range(N):
-        start = time.perf_counter()
-        G = data.get_graph_bellman_ford()
-        bellman_ford(G, 0)
-        end = time.perf_counter()
-        time_sec += end-start
-    print(f"ベルマンフォード: {time_sec/N:0.7f} sec")
+    G = data.get_graph_bellman_ford()
+    start = time.perf_counter()
+    bellman_ford(G, 0)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"ベルマンフォード: {time_sec:0.7f} sec")
     
     # spfa
-    time_sec = 0
-    for i in range(N):
-        start = time.perf_counter()
-        G = data.get_graph_with_weight()
-        spfa(G, 0)
-        end = time.perf_counter()
-        time_sec += end-start
-    print(f"spfa: {time_sec/N:0.7f} sec")
+    G = data.get_graph_with_weight()
+    start = time.perf_counter()
+    spfa(G, 0)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"spfa: {time_sec:0.7f} sec")
     
-    # 全対最短経路
-    print("全対最短経路")
+    # 全点対最短経路
+    print("全点対最短経路")
     
     # ワーシャルフロイド法
-    start = time.perf_counter()
     G = data.get_graph_matrix()
+    start = time.perf_counter()
     floyd_warshall(G)
     end = time.perf_counter()
-    print(f"ワーシャルフロイド法: {end-start:0.7f} sec")
+    time_sec = end-start
+    print(f"ワーシャルフロイド法: {time_sec:0.7f} sec")
     
     # 全点ダイクストラ(二分ヒープ)
-    start = time.perf_counter()
     G = data.get_graph_with_weight()
+    start = time.perf_counter()
     for i in range(data.nodes):
         dijkstra(G, i)
     end = time.perf_counter()
-    print(f"全点ダイクストラ(二分ヒープ): {end-start:0.7f} sec")
+    time_sec = end-start
+    print(f"ダイクストラ(二分ヒープ): {time_sec:0.7f} sec")
     
     # 全点ダイクストラ(隣接行列)
-    start = time.perf_counter()
     G = data.get_graph_matrix()
+    start = time.perf_counter()
     for i in range(data.nodes):
         dijkstra(G, i, PQ=False)
     end = time.perf_counter()
-    print(f"全点ダイクストラ(隣接行列): {end-start:0.7f} sec")
-    
-    # 全点spfa
-    start = time.perf_counter()
-    G = data.get_graph_with_weight()
-    for i in range(data.nodes):
-        spfa(G, i)
-    end = time.perf_counter()
-    print(f"全点spfa: {end-start:0.7f} sec")
+    time_sec = end-start
+    print(f"ダイクストラ(隣接行列): {time_sec:0.7f} sec")
     
     # 全点ベルマンフォード
-    start = time.perf_counter()
     G = data.get_graph_bellman_ford()
+    start = time.perf_counter()
     for i in range(data.nodes):
         bellman_ford(G, i)
     end = time.perf_counter()
-    print(f"全点ベルマンフォード: {end-start:0.7f} sec")
+    time_sec = end-start
+    print(f"ベルマンフォード: {time_sec:0.7f} sec")
+    
+    # 全点spfa
+    G = data.get_graph_with_weight()
+    start = time.perf_counter()
+    for i in range(data.nodes):
+        spfa(G, i)
+    end = time.perf_counter()
+    time_sec = end-start
+    print(f"spfa: {time_sec:0.7f} sec")
     
 
 if __name__ == "__main__":
